37588a472832426a6b788fdb110a5b2934a99c1b
Alex Rocha <alexrocha@MacBook-Air-de-Alex.local>
2022-11-28 00:34:14 -0300

    feat: commit codebase

---
+++ index.js
@@ -0,0 +1,7 @@
+console.debug('Note`s CLI');
+
+console.table('----------------------------------');
+console.table('------------Note`s CLI------------');
+console.table('----------------------------------\n');
+
+console.debug('----------------------------------');
