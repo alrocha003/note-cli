ce7140e41420a894a713164ee2b7d5d97cb832d2
Alex Rocha <alexrocha@MacBook-Air-de-Alex.local>
2022-11-28 23:59:15 -0300

    feat: implement code to save data in file db

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
-console.debug('Note`s CLI');
+#!/usr/bin/env node
+import * as fs from 'fs';
+
+const FILE_NAME = 'db';
+
 
 console.table('----------------------------------');
 console.table('------------Note`s CLI------------');
 console.table('----------------------------------\n');
 
-console.debug('----------------------------------');
+
+if (process?.argv[2]?.indexOf('--add') > -1)
+    fs.appendFile(`./${FILE_NAME}.txt`, process.argv[3], (err, file) => {
+        if (err) console.error(err);
+    });
+else
+    fs.readFile(`./${FILE_NAME}.txt`, (error, data) => {
+        if (error) {
+            console.error(`Não foi possível ler os dados da base: ${error}`);
+            return;
+        }
+        console.debug(data.toString('utf8'));
+    });
+
+
+
+
